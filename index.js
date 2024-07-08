09c9dbf0e2ce04c03b212d2381ea2dec5d78a50f
Prawito Hudoro <prawitohudoro@gmail.com>
2024-07-08 11:07:21 +0700

    perbaikan const ke 3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const A = "Perubahan ke 1 - hotfix juga";
 const B = "Perubahan ke 2 - hotfix";
-const C = "Perubahan ke 3";
+const C = "Perubahan ke 3 - hotfix ke 3";
 const D = "Perubahan ke 4";
 const E = "Perubahan ke 5";
 const F = "Perubahan ke 6";
